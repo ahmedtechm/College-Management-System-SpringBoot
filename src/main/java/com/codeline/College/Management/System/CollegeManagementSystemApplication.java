3291816b2ca3c9d24420015c9a5f2a7d25993404
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-06 23:43:44 +0400

    Added College Management System Spring Boot Project to GitHub

---
+++ src/main/java/com/codeline/College/Management/System/CollegeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codeline.College.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeline/College/Management/System/CollegeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeline.College.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
