099aa234bef4539aa41a9a59e18897f9766b8683
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-08 14:37:40 +0400

    Added  @Getter @Setter @Data @Entity annotations for Student Class

--- src/main/java/com/codeline/College/Management/System/Models/Student.java
+++ src/main/java/com/codeline/College/Management/System/Models/Student.java
@@ -1,8 +1,18 @@
 package com.codeline.College.Management.System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+@Getter
+@Setter
+@Data
+@Entity
 
 public class Student {
     @Id
