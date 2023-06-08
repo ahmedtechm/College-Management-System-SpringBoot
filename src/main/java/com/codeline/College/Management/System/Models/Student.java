7b6e22ddb9d6c7b584e0fcc9f3a81ad5bebd74d6
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-08 14:27:38 +0400

    Added @Id annotation to Student class for primary key identification

--- src/main/java/com/codeline/College/Management/System/Models/Student.java
+++ src/main/java/com/codeline/College/Management/System/Models/Student.java
@@ -1,6 +1,9 @@
 package com.codeline.College.Management.System.Models;
 
+import javax.persistence.Id;
+
 public class Student {
+    @Id
     private Long id;
     private String name;
     private String email;
