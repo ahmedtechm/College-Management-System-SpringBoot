953b6f8cba3be2c35b4e25e72454b6ae708e7dee
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-08 14:23:22 +0400

    Added Student class with basic fields: id, name, email, and course

--- src/main/java/com/codeline/College/Management/System/Models/Student.java
+++ src/main/java/com/codeline/College/Management/System/Models/Student.java
@@ -1,5 +1,8 @@
 package com.codeline.College.Management.System.Models;
 
 public class Student {
-
+    private Long id;
+    private String name;
+    private String email;
+    private String course;
 }
