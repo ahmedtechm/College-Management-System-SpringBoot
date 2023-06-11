10b62520a9638a31c46341235db27194308a5797
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-11 14:36:36 +0400

    Create a new student with initial details

--- src/main/java/com/codeline/College/Management/System/Service/StudentService.java
+++ src/main/java/com/codeline/College/Management/System/Service/StudentService.java
@@ -5,10 +5,22 @@ import com.codeline.College.Management.System.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class StudentService {
 
     @Autowired
     StudentRepository studentRepository;
 
+
+    public void createStudent() {
+        Student student = new Student();
+        student.setName("AHMED");
+        student.setEmail("ahmed@gmail.com");
+        student.setCourse("Java");
+        student.setCreatedDate(new Date());
+        student.setIsActive(true);
+        studentService.saveEmployee(student);
+    }
 }
