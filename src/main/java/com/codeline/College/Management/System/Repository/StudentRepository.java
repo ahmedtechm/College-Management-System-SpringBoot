141d6a85ea5096178c031cd8fb77a9621db07859
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-11 09:25:36 +0400

    Added JpaRepository interface extension for Student entity

--- src/main/java/com/codeline/College/Management/System/Repository/StudentRepository.java
+++ src/main/java/com/codeline/College/Management/System/Repository/StudentRepository.java
@@ -1,8 +1,10 @@
 package com.codeline.College.Management.System.Repository;
 
+import com.codeline.College.Management.System.Models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
