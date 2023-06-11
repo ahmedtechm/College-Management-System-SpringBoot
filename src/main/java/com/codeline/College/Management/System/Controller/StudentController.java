40ef4c9a789c236cf9723304145dc46cfc17781a
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-11 13:44:45 +0400

    Added dependency injection for StudentService in StudentController class

--- src/main/java/com/codeline/College/Management/System/Controller/StudentController.java
+++ src/main/java/com/codeline/College/Management/System/Controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.codeline.College.Management.System.Controller;
 
+import com.codeline.College.Management.System.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+    @Autowired
+    StudentService studentService;
 
 }
