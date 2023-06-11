d5b12becd0cb73d8102f2007ed6e06ce5a80d053
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-11 14:00:43 +0400

    Added @Autowired annotation to StudentRepository

--- src/main/java/com/codeline/College/Management/System/Service/StudentService.java
+++ src/main/java/com/codeline/College/Management/System/Service/StudentService.java
@@ -1,8 +1,13 @@
 package com.codeline.College.Management.System.Service;
 
+import com.codeline.College.Management.System.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
 
 }
