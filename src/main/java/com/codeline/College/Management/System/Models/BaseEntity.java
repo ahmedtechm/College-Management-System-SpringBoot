4097ae440f44d07f9ccf07cf2ab4477a67574cef
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-11 14:41:18 +0400

    Added @MappedSuperclass @Getter @Setter @Data annotations to BaseEntity class

--- src/main/java/com/codeline/College/Management/System/Models/BaseEntity.java
+++ src/main/java/com/codeline/College/Management/System/Models/BaseEntity.java
@@ -1,7 +1,15 @@
 package com.codeline.College.Management.System.Models;
 
-import java.util.Date;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+@MappedSuperclass
+@Getter
+@Setter
+@Data
 public class BaseEntity {
     Date createdDate;
     Date updatedDate;
