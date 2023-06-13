4648a336ec94a9372af75b8ca46c79f0c4dc8a4e
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-13 23:46:50 +0400

    Implement student creation endpoint

--- src/main/java/com/codeline/College/Management/System/Controller/StudentController.java
+++ src/main/java/com/codeline/College/Management/System/Controller/StudentController.java
@@ -2,6 +2,7 @@ package com.codeline.College.Management.System.Controller;
 
 import com.codeline.College.Management.System.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +10,9 @@ public class StudentController {
     @Autowired
     StudentService studentService;
 
+
+    @RequestMapping("student/create")
+    public void saveStudent() {
+        studentService.createStudent();
+    }
 }
