3dd2399160ee034e92bccc4f421bd48009085ef7
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 00:46:00 +0400

    Added @getter @Setter @DaTa @entity annotations for Professor Class

--- src/main/java/com/codeline/College/Management/System/Models/Professor.java
+++ src/main/java/com/codeline/College/Management/System/Models/Professor.java
@@ -1,10 +1,21 @@
 package com.codeline.College.Management.System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
+@Setter
+@Data
+@Entity
+
 public class Professor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
