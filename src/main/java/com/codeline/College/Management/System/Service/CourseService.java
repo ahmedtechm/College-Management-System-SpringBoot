377eb39a9c6630f3ea45cfbdddf6a112c6c73b83
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-14 23:10:06 +0400

    Added @Autowired annotation to CourseRepository

--- src/main/java/com/codeline/College/Management/System/Service/CourseService.java
+++ src/main/java/com/codeline/College/Management/System/Service/CourseService.java
@@ -1,8 +1,12 @@
 package com.codeline.College.Management.System.Service;
 
+import com.codeline.College.Management.System.Repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
 
+    @Autowired
+    CourseRepository courseRepository;
 }
