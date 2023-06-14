8fc73db0035878d42c259477471c40367b059596
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-14 22:53:43 +0400

    Added JpaRepository interface extension for Course entity

--- src/main/java/com/codeline/College/Management/System/Repository/CourseRepository.java
+++ src/main/java/com/codeline/College/Management/System/Repository/CourseRepository.java
@@ -1,8 +1,10 @@
 package com.codeline.College.Management.System.Repository;
 
+import com.codeline.College.Management.System.Models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
