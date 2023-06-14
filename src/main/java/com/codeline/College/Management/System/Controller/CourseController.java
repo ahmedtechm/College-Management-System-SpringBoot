a79a247f7d4b707ed48911f46266cbc23062eaa1
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 00:14:01 +0400

    Implement course creation endpoint

--- src/main/java/com/codeline/College/Management/System/Controller/CourseController.java
+++ src/main/java/com/codeline/College/Management/System/Controller/CourseController.java
@@ -2,6 +2,7 @@ package com.codeline.College.Management.System.Controller;
 
 import com.codeline.College.Management.System.Service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,8 @@ public class CourseController {
     @Autowired
     CourseService courseService;
 
+    @RequestMapping("course/create")
+    public void saveCourse() {
+        courseService.createCourse();
+    }
 }
