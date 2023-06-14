615b026d103743164dbe71bbeffd4ebe16495ce5
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 00:38:32 +0400

    Added Professor class with basic fields:id, name, phone,email and specialization

--- src/main/java/com/codeline/College/Management/System/Models/Professor.java
+++ src/main/java/com/codeline/College/Management/System/Models/Professor.java
@@ -2,4 +2,10 @@ package com.codeline.College.Management.System.Models;
 
 public class Professor {
 
+    Long id;
+    String name;
+    String phone;
+    String email;
+    String specialization;
+
 }
