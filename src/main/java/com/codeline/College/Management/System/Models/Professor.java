ddcf483b122ee14c90c0a3f6972df31f1d73e651
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 00:48:31 +0400

    Added column mapping for 'prosessor_name'

--- src/main/java/com/codeline/College/Management/System/Models/Professor.java
+++ src/main/java/com/codeline/College/Management/System/Models/Professor.java
@@ -4,10 +4,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -19,9 +16,14 @@ public class Professor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(name = "professor_name")
     String name;
+
     String phone;
+
     String email;
+
     String specialization;
 
 }
