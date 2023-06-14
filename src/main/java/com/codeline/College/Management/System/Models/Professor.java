3f2f541ca00d3b091edb5dafc0ec9ff1e029ce19
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 00:43:27 +0400

    Added identity generation strategy to Professor class

--- src/main/java/com/codeline/College/Management/System/Models/Professor.java
+++ src/main/java/com/codeline/College/Management/System/Models/Professor.java
@@ -1,9 +1,12 @@
 package com.codeline.College.Management.System.Models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Professor {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String name;
     String phone;
