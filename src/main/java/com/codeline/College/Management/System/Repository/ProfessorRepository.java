55d251f64d3222080bd0bfc398ab107355226ec0
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 00:52:52 +0400

    Added JpaRepository interface extension for Professor entity

--- src/main/java/com/codeline/College/Management/System/Repository/ProfessorRepository.java
+++ src/main/java/com/codeline/College/Management/System/Repository/ProfessorRepository.java
@@ -1,8 +1,10 @@
 package com.codeline.College.Management.System.Repository;
 
+import com.codeline.College.Management.System.Models.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProfessorRepository {
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 
 }
