e22b4d31acfd00cd6c742565d27a151b88b7279f
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 01:19:34 +0400

    Create a new professor  with initial details

--- src/main/java/com/codeline/College/Management/System/Service/ProfessorService.java
+++ src/main/java/com/codeline/College/Management/System/Service/ProfessorService.java
@@ -1,8 +1,11 @@
 package com.codeline.College.Management.System.Service;
 
+import com.codeline.College.Management.System.Models.Professor;
 import com.codeline.College.Management.System.Repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class ProfessorService {
@@ -10,6 +13,15 @@ public class ProfessorService {
     @Autowired
     ProfessorRepository professorRepository;
 
+    public void createProfessor() {
+        Professor professor = new Professor();
+        professor.setName("Daniyal");
+        professor.setPhone("99352234");
+        professor.setEmail("daniyal@codeline.com");
+        professor.setSpecialization("JAVA developer");
+        professor.setCreatedDate(new Date());
+        professor.setIsActive(true);
+        ProfessorRepository.save(professor);
 
-
+    }
 }
