2ed507598b1c299f8810a074abbb9c45e272e2d9
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 00:06:23 +0400

    Create a new course  with initial details

--- src/main/java/com/codeline/College/Management/System/Service/CourseService.java
+++ src/main/java/com/codeline/College/Management/System/Service/CourseService.java
@@ -1,12 +1,24 @@
 package com.codeline.College.Management.System.Service;
 
+import com.codeline.College.Management.System.Models.Course;
 import com.codeline.College.Management.System.Repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class CourseService {
 
     @Autowired
     CourseRepository courseRepository;
+
+    public void createCourse() {
+        Course course = new Course();
+        course.setName("Computer Science");
+        course.setCreatedDate(new Date());
+        course.setIsActive(true);
+        courseRepository.save(course);
+
+    }
 }
