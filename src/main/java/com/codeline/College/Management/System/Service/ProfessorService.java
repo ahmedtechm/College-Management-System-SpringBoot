214e1f771d59b16db5aa536c0006b906387ca700
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 01:03:21 +0400

    Added @Autowired annotation to ProfessorRepository

--- src/main/java/com/codeline/College/Management/System/Service/ProfessorService.java
+++ src/main/java/com/codeline/College/Management/System/Service/ProfessorService.java
@@ -1,8 +1,13 @@
 package com.codeline.College.Management.System.Service;
 
+import com.codeline.College.Management.System.Repository.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfessorService {
+
+    @Autowired
+    ProfessorRepository professorRepository;
 
 }
