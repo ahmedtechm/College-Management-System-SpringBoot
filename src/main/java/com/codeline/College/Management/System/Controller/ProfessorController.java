4bc47c1b87dc56eba4780bc68cea466c2c67639d
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-15 01:26:22 +0400

    Added dependency injection for ProfessorService in ProfessorController class

--- src/main/java/com/codeline/College/Management/System/Controller/ProfessorController.java
+++ src/main/java/com/codeline/College/Management/System/Controller/ProfessorController.java
@@ -1,8 +1,13 @@
 package com.codeline.College.Management.System.Controller;
 
+import com.codeline.College.Management.System.Service.ProfessorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProfessorController {
+
+    @Autowired
+    ProfessorService professorService;
 
 }
