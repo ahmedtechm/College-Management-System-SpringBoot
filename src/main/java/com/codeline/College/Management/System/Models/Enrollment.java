cf9acedd2ad840fd53faf60bdc85f1f3533625fa
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-15 14:14:14 +0400

    Added identity generation strategy to Enrollment class

--- src/main/java/com/codeline/College/Management/System/Models/Enrollment.java
+++ src/main/java/com/codeline/College/Management/System/Models/Enrollment.java
@@ -1,9 +1,12 @@
 package com.codeline.College.Management.System.Models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Enrollment {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     String studentName;
