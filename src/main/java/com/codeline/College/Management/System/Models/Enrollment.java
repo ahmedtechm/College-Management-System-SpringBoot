4f4755c4608ff7f52dd0c3a049b425798c952a95
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-15 16:51:50 +0400

    Added table name annotation for enrollments table

--- src/main/java/com/codeline/College/Management/System/Models/Enrollment.java
+++ src/main/java/com/codeline/College/Management/System/Models/Enrollment.java
@@ -4,14 +4,13 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 @Getter
 @Setter
 @Data
 @Entity
+@Table(name = "enrollments")
 
 public class Enrollment {
     @Id
