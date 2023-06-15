59212476479911f92c183fbf61a8e399e11b5060
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-15 16:48:49 +0400

    Added JpaRepository interface extension for Enrollment entity

--- src/main/java/com/codeline/College/Management/System/Repository/EnrollmentRepository.java
+++ src/main/java/com/codeline/College/Management/System/Repository/EnrollmentRepository.java
@@ -1,8 +1,10 @@
 package com.codeline.College.Management.System.Repository;
 
+import com.codeline.College.Management.System.Models.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnrollmentRepository {
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
 }
