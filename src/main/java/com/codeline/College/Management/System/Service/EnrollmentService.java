5faa246387d4b25a716bb372921454083b077ec7
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-15 16:56:40 +0400

    Added @Autowired annotation to EnrollmentRepository

--- src/main/java/com/codeline/College/Management/System/Service/EnrollmentService.java
+++ src/main/java/com/codeline/College/Management/System/Service/EnrollmentService.java
@@ -1,8 +1,13 @@
 package com.codeline.College.Management.System.Service;
 
+import com.codeline.College.Management.System.Repository.EnrollmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EnrollmentService {
+
+    @Autowired
+    EnrollmentRepository enrollmentRepository;
 
 }
